b8a98ce8023e9d0eb54130aaf494d81a4f3ad0c9
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2023-04-28 16:38:17 +0200

    added the view for the index

--- adulting/budgets/views.py
+++ adulting/budgets/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return render(request, "users/login.html", {
+                "message": "Access denied. You need to log in first."
+            })
+    return render(request, "budgets/index.html")
