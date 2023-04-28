40fe17ca922c702292473bf61470a1d9a05ca509
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2023-04-28 18:33:45 +0200

    added the models to admin.py. Makes it easier to add some data.

--- adulting/budgets/admin.py
+++ adulting/budgets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Budget, Category, Entry
+
 # Register your models here.
+admin.site.register(Budget)
+admin.site.register(Category)
+admin.site.register(Entry)
