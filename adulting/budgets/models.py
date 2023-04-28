24e1b705092896c0ef6155470d571caf20c451e5
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2023-04-28 18:32:35 +0200

    created all the necessary models for the budget app.

--- adulting/budgets/models.py
+++ adulting/budgets/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Budget(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    budget_text = models.CharField("Description", max_length=200)
+    funds = models.IntegerField("Initial Funds")
+    start_date = models.DateField("Start Date")
+    end_date = models.DateField("End Date")
+
+class Category(models.Model):
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    category_text = models.CharField("Description", max_length=200)
+
+class Entry(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    entry_text = models.CharField("Description", max_length=200)
+    costs = models.IntegerField()
