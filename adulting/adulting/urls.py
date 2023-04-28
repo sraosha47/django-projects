bf264da25ccb1f39cd7b2bbad8889c5ba7d36487
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2023-04-28 16:31:03 +0200

    added budgets' urls.py to adulting's urls.py

--- adulting/adulting/urls.py
+++ adulting/adulting/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("users/", include("users.urls")),
+    path('budgets/', include('budgets.urls')),
     path('admin/', admin.site.urls),
-    path("users/", include("users.urls"))
+
 ]
