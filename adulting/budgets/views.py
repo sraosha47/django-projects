1e76a82e60eaaccc491481a137988db21c803c5f
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2023-04-29 16:09:51 +0200

    tried writing a basic view checking authorization and giving a list of all budgets. Didn't work. A generic view worked, but withouth authorization check

--- adulting/budgets/views.py
+++ adulting/budgets/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
+#from django.shortcuts import render, get_object_or_404
+#from django.urls import reverse
+from django.views import generic
+
+
+from .models import Budget
+#from django.contrib.auth.models import User 
 
 # Create your views here.
+"""
 def index(request):
     if not request.user.is_authenticated:
         return render(request, "users/login.html", {
                 "message": "Access denied. You need to log in first."
             })
-    return render(request, "budgets/index.html")
+    else:
+        budget_list = Budget.objects.all()
+        context = {
+            "budget_list": budget_list,
+        }
+        return render(request, context, "budgets/index.html")
+"""
+
+class IndexView(generic.ListView):
+    template_name = "budgets/index.html"
+    context_object_name = "budget_list"
+
+    def get_queryset(self):
+        return Budget.objects.all()  
