5eb8fdc7e888c2d62a39e062ef338e56e81342e0
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2023-04-29 20:13:50 +0200

    changed the path for the index so the username is added

--- adulting/budgets/urls.py
+++ adulting/budgets/urls.py
@@ -6,6 +6,8 @@ from . import views
 app_name = "budgets"
 
 urlpatterns = [
-    path("", login_required(views.IndexView.as_view()) , name="index"),
-    
+    #path("", login_required(views.IndexView.as_view()) , name="index"),
+    path("<str:username>", views.index, name="index"),
+    path("<int:pk>/<str:name>/", login_required(views.CategoriesView.as_view()), name="categories"),
+    path("<int:pk>/entries", login_required(views.EntriesView.as_view()), name="entries"),
 ]
