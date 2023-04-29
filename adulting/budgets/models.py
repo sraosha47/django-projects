10d57f032e09f26adea42a50dcd3ecc8762b181b
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2023-04-29 14:45:26 +0200

    defined a different string output for the different tables to improve readability in Django Admin

--- adulting/budgets/models.py
+++ adulting/budgets/models.py
@@ -9,11 +9,20 @@ class Budget(models.Model):
     start_date = models.DateField("Start Date")
     end_date = models.DateField("End Date")
 
+    def __str__(self):
+        return self.budget_text
+
 class Category(models.Model):
     budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
     category_text = models.CharField("Description", max_length=200)
+
+    def __str__(self):
+        return self.category_text
 
 class Entry(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     entry_text = models.CharField("Description", max_length=200)
     costs = models.IntegerField()
+
+    def __str__(self):
+        return self.entry_text
