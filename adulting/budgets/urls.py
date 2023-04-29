b671424057525799d35b2f6fa783e8506b449330
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2023-04-29 16:18:32 +0200

    found another way to make login a requirement. All haik to Django

--- adulting/budgets/urls.py
+++ adulting/budgets/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required, permission_required
+
 from . import views
 
 app_name = "budgets"
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
+    path("", login_required(views.IndexView.as_view()) , name="index"),
     
 ]
