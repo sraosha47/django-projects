1c1f2ee1ab3ad68861849fe8bab9c1e7fdd1105b
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2023-04-30 14:53:16 +0200

    added id to the respective urls

--- adulting/budgets/urls.py
+++ adulting/budgets/urls.py
@@ -7,6 +7,6 @@ app_name = "budgets"
 
 urlpatterns = [
     path("<str:username>", views.index, name="index"),
-    path("categories/<str:username>/<str:budget_name>", views.categories, name="categories"),
-    path("entries/<str:username>/<str:category_name>", views.entries, name="entries"),
+    path("categories/<str:username>/<str:budget_name>/<int:budget_id>", views.categories, name="categories"),
+    path("entries/<str:username>/<str:category_name>/<int:category_id>", views.entries, name="entries"),
 ]
