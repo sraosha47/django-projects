fb82eecfeedf66f75b021f0e512993ef14fa5a8d
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2023-04-30 17:22:12 +0200

    added the path for the account view

--- adulting/users/urls.py
+++ adulting/users/urls.py
@@ -6,5 +6,6 @@ app_name = "users"
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout")
+    path("logout", views.logout_view, name="logout"),
+    path("account/<str:username>", views.account_view, name="account")
 ]
