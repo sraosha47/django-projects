8b9a9ca713c594621b05db5aa3a68bbb184745d4
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2023-04-30 14:54:19 +0200

    changed the depiction of all the models on the admin page

--- adulting/budgets/admin.py
+++ adulting/budgets/admin.py
@@ -3,6 +3,29 @@ from django.contrib import admin
 from .models import Budget, Category, Entry
 
 # Register your models here.
-admin.site.register(Budget)
-admin.site.register(Category)
-admin.site.register(Entry)
+class CategoryInLine(admin.TabularInline):
+    model = Category
+    extra = 3
+
+class EntryInLine(admin.TabularInline):
+    model = Entry
+    extra = 3
+
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ["budget_text", "owner" ]
+    fieldsets = [
+        ("Description", {"fields": ["owner", "budget_text"]}),
+        ("Funds", {"fields": ["funds"]}),
+        ("Timeperiod", {"fields": ["start_date", "end_date"]})
+    ]
+    inlines = [CategoryInLine]
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["category_text", "budget"]
+    fieldsets = [
+        (None, {"fields": ["budget", "category_text"]})
+    ]
+    inlines = [EntryInLine]
+
+admin.site.register(Budget, BudgetAdmin)
+admin.site.register(Category, CategoryAdmin)
