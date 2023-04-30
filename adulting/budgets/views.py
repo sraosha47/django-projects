d240208c99fb430fb2939e461e229859176684ef
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2023-04-30 02:56:11 +0200

    rewrote all the views to change the url more to my liking and to ensure only the owner has access to data

--- adulting/budgets/views.py
+++ adulting/budgets/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.contrib.auth.models import User
 
-from .models import Budget, Category
+from .models import Budget, Category, Entry
 
 # Create your views here.
 
@@ -24,10 +24,40 @@ def index(request, username):
         }
         return render(request, "budgets/index.html", context)
 
-class CategoriesView(generic.DetailView):
-    model = Budget
-    template_name = "budgets/categories.html"
+def categories(request, budget_name, username):
+    if not request.user.is_authenticated:
+        return render(request, "users/login.html", {
+                "message": "Access denied. You need to log in first."
+            })
+    elif not username == request.user.username:
+        return render(request, "budgets/index.html", {
+            "message": "Access denied. These are not the budgets you're looking for."
+        })
+    else:
+        budget_id = Budget.objects.filter(budget_text=budget_name)[0].id
+        category_list = Category.objects.filter(budget_id=budget_id)
+        budget_name=budget_name
+        context = {
+            "category_list": category_list,
+            "budget_name": budget_name
+        }
+        return render(request, "budgets/categories.html", context)
 
-class EntriesView(generic.DetailView):
-    model = Category
-    template_name = "budgets/entries.html"
+def entries(request, category_name, username):
+    if not request.user.is_authenticated:
+        return render(request, "users/login.html", {
+                "message": "Access denied. You need to log in first."
+            })
+    elif not username == request.user.username:
+        return render(request, "budgets/index.html", {
+            "message": "Access denied. These are not the budgets you're looking for."
+        })
+    else:
+        category_id = Category.objects.filter(category_text=category_name)[0].id
+        entry_list = Entry.objects.filter(category_id=category_id)
+        category_name=category_name
+        context = {
+            "entry_list": entry_list,
+            "category_name": category_name
+        }
+        return render(request, "budgets/entries.html", context)
