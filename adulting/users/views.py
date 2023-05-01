7c34454c2c44045a7801616f10986251ce221811
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2023-05-02 01:23:06 +0200

    added some checks to the account view. Defined a view allowing users to change their info and/or password. Changed the name of the view that leads to the form to create a new user and defined a view that allows said creation.

--- adulting/users/views.py
+++ adulting/users/views.py
@@ -25,7 +25,7 @@ def login_view(request):
             })
     else:
         return render(request, "users/login.html")
-
+    
 def logout_view(request):
     logout(request)
     return render(request, "users/login.html", {
@@ -33,17 +33,76 @@ def logout_view(request):
     })
 
 def account_view(request, username):
-    account = get_object_or_404(User, pk=request.user.id)
-    context ={
-        "account": account,
-        "username": username
-    }
-    return render(request, "users/account.html", context)
+    if not request.user.is_authenticated:
+        return render(request, "users/login.html", {
+                "message": "Access denied. You need to log in first."
+            })
+    elif not username == request.user.username and not request.user.is_superuser:
+        return render(request, "users/user.html", {
+            "message": "Access denied. That's not your account."
+        })
+    else:
+        account = get_object_or_404(User, pk=request.user.id)
+        context ={
+            "account": account,
+            "username": username
+        }
+        return render(request, "users/account.html", context)
 
-def create_view(request):
+def change_view(request):
+    if request.method == "POST":
+        user_id = request.user.id
+        new = None
+        new = User.objects.filter(id=user_id).get()
+        username = request.POST["username"] 
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]  
+        if len(username) > 0:
+            new.username = username
+            new.first_name = request.POST["first_name"]
+            new.last_name = request.POST["last_name"]
+            new.email = request.POST["email"] 
+            if password != confirmation:
+                return render(request, "users/user.html", { "message": "Passwords do not match."} )
+            elif len(password) > 0 :
+                new.set_password(password)
+                new.save()
+                return HttpResponseRedirect(reverse("users:login"))
+            else:
+                new.save()
+                return HttpResponseRedirect(reverse("users:index"))
+        else:
+            context ={
+                "message": "Username is required"
+            } 
+            return render(request, "users/user.html", context)
+    else:
+        return render(request, "users/user.html")
+
+def newuser_view(request):
     if request.user.is_superuser:
         return render(request, "users/new_user.html")
     else:
         return render(request, "users/user.html", {
             "message": "You shall not pass."
         })
+    
+def create_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        if username is not None and password is not None:
+            first_name = request.POST["first_name"]
+            last_name = request.POST["last_name"]
+            email = request.POST["email"]   
+            user = User.objects.create_user(username, password)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.email = email
+            return HttpResponseRedirect(reverse("users:index"))
+        else:
+            return render(request, "users/new_user.html", {
+                "message": "Username and Password not provided."
+            })
+    else:
+        return render(request, "users/new_user.html")
