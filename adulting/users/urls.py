1ce156c17802028778dd262456750e23d41bff04
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2023-05-01 22:05:56 +0200

    added a path for the create view

--- adulting/users/urls.py
+++ adulting/users/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
+    path("newuser", views.create_view, name="create"),
     path("<str:username>", views.account_view, name="account")
 ]
