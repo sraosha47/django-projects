47b706fd99236f0298dd05c005f691728f29ed66
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2023-05-02 03:02:14 +0200

    changed the url of newuser and added the create path.

--- adulting/users/urls.py
+++ adulting/users/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("newuser", views.newuser_view, name="create"),
+    path("account/newuser", views.newuser_view, name="newuser"),
+    path("account/created", views.create_view, name="create"),
     path("account/changed", views.change_view, name="change"),
     path("account/<str:username>", views.account_view, name="account"),
 ]
