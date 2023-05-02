a0658564704fc08882ae386af46244a500880141
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2023-05-02 03:01:16 +0200

    removed conditional from change view, as the require attribute made it unnecessary. added authentication check to newuser view. Finished the create view.

--- adulting/users/views.py
+++ adulting/users/views.py
@@ -57,30 +57,28 @@ def change_view(request):
         username = request.POST["username"] 
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]  
-        if len(username) > 0:
-            new.username = username
-            new.first_name = request.POST["first_name"]
-            new.last_name = request.POST["last_name"]
-            new.email = request.POST["email"] 
-            if password != confirmation:
-                return render(request, "users/user.html", { "message": "Passwords do not match."} )
-            elif len(password) > 0 :
-                new.set_password(password)
-                new.save()
-                return HttpResponseRedirect(reverse("users:login"))
-            else:
-                new.save()
-                return HttpResponseRedirect(reverse("users:index"))
+        new.username = username
+        new.first_name = request.POST["first_name"]
+        new.last_name = request.POST["last_name"]
+        new.email = request.POST["email"] 
+        if password != confirmation:
+            return render(request, "users/user.html", { "message": "Passwords do not match."} )
+        elif len(password) > 0 :
+            new.set_password(password)
+            new.save()
+            return HttpResponseRedirect(reverse("users:login"))
         else:
-            context ={
-                "message": "Username is required"
-            } 
-            return render(request, "users/user.html", context)
+            new.save()
+            return HttpResponseRedirect(reverse("users:index"))
     else:
         return render(request, "users/user.html")
 
 def newuser_view(request):
-    if request.user.is_superuser:
+    if not request.user.is_authenticated:
+        return render(request, "users/login.html", {
+                "message": "Access denied. You need to log in first."
+            })
+    elif request.user.is_superuser:
         return render(request, "users/new_user.html")
     else:
         return render(request, "users/user.html", {
@@ -89,20 +87,24 @@ def newuser_view(request):
     
 def create_view(request):
     if request.method == "POST":
+        superuser = request.POST["superuser"] 
         username = request.POST["username"]
         password = request.POST["password"]
-        if username is not None and password is not None:
-            first_name = request.POST["first_name"]
-            last_name = request.POST["last_name"]
-            email = request.POST["email"]   
-            user = User.objects.create_user(username, password)
-            user.first_name = first_name
-            user.last_name = last_name
-            user.email = email
-            return HttpResponseRedirect(reverse("users:index"))
-        else:
-            return render(request, "users/new_user.html", {
-                "message": "Username and Password not provided."
-            })
+        confirmation = request.POST["confirmation"]   
+        if password == confirmation:
+            if superuser == "YES":
+                new_usr = User.objects.create_superuser(username, password=password)
+                new_usr.first_name = request.POST["first_name"]
+                new_usr.last_name = request.POST["last_name"]
+                new_usr.email = request.POST["email"]
+                new_usr.save()
+                return HttpResponseRedirect(reverse("users:newuser"))
+            else:
+                new_usr = User.objects.create_user(username, password=password)
+                new_usr.first_name = request.POST["first_name"]
+                new_usr.last_name = request.POST["last_name"]
+                new_usr.email = request.POST["email"]
+                new_usr.save()
+                return HttpResponseRedirect(reverse("users:newuser"))
     else:
         return render(request, "users/new_user.html")
