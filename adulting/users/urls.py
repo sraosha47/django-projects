93e5e139325f24238dcc40c5073042de58edd363
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2023-05-08 22:42:54 +0200

    added a path for the register view.

--- adulting/users/urls.py
+++ adulting/users/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
+    path("register", views.register_view, name="register"),
     path("account/newuser", views.newuser_view, name="newuser"),
     path("account/created", views.create_view, name="create"),
     path("account/changed", views.change_view, name="change"),
